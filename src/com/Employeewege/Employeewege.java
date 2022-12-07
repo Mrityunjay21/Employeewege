091de61f4ea1a6562648ce248f1a73b232c58d5f
Mrityunjay21 <115720588+Mrityunjay21@users.noreply.github.com>
2022-12-07 16:22:09 +0530

    Calculating Month wege

--- src/com/Employeewege/Employeewege.java
+++ src/com/Employeewege/Employeewege.java
@@ -84,3 +84,40 @@ public class Employeewege {
 		
 	}
 }
+
+package com.Employeewege;
+//UC5Calculating month wege
+public class Employeewege {
+ 
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_WORKING_DAYS = 20;
+	
+	
+	public static void main(String[] args) {
+		
+		int empHrs = 0;
+		int empWege = 0;
+		int totalempWege = 0;
+		for (int day = 0;day <NUM_WORKING_DAYS;day++) {
+		int empCheck = (int)Math.floor(Math.random()*10%3);
+		switch (empCheck) {
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		default:
+			empHrs = 0;
+				
+		}
+		
+		empWege = empHrs * EMP_RATE_PER_HOUR;
+		totalempWege+= empWege;
+		System.out.println("Employee wege is : " + totalempWege);
+		
+	}
+}
+}
