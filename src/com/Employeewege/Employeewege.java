a2d776f04101cf25e01eb1adbaa05c57022955db
Mrityunjay21 <115720588+Mrityunjay21@users.noreply.github.com>
2022-12-07 14:31:42 +0530

    Calculate part & full time

--- src/com/Employeewege/Employeewege.java
+++ src/com/Employeewege/Employeewege.java
@@ -31,3 +31,25 @@ public class Employeewege {
 		
 	}
 }
+package com.Employeewege;
+//UC3
+public class Employeewege {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs = 0;
+		int empWege = 0;
+		double empCheck = Math.floor(Math.random()*10%3);
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		else 
+			 empHrs = 0;
+		empWege = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee wege: " + empWege);
+		
+	}
+}
