45d72e84f890613cf93b7b0d6dca5d0483d7d148
Mrityunjay21 <115720588+Mrityunjay21@users.noreply.github.com>
2022-12-07 14:01:40 +0530

    Calculate empWege per hour

--- src/com/Employeewege/Employeewege.java
+++ src/com/Employeewege/Employeewege.java
@@ -11,3 +11,23 @@ public class Employeewege {
 			System.out.println("Employee is Absent");
 	}
 }
+
+package com.Employeewege;
+//UC2
+public class Employeewege {
+
+	public static void main(String[] args) {
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs = 0;
+		int empWege = 0;
+		double empCheck = Math.floor(Math.random()*10%2);
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else 
+			 empHrs = 0;
+		empWege = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee wege: " + empWege);
+		
+	}
+}
